ea19a7f336a232b470cc10240c5ed7b43def39e4
smashou <v.gache@gmail.com>
2015-02-14 16:29:20 +0100

    Add data fixtures

---
+++ src/AppBundle/DataFixtures/ORM/FixturesLoader.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Hautelook\AliceBundle\Alice\DataFixtureLoader;
+use Nelmio\Alice\Fixtures;
+
+class FixturesLoader extends DataFixtureLoader
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getFixtures()
+    {
+        return  array(
+            __DIR__ . '/Fixtures/blogs.yml',
+        );
+    }
+}
