a44188aa3c31e6cecaee773c683583a22ae7f2ca
smashou <v.gache@gmail.com>
2015-02-21 17:13:33 +0100

    Introduce voter for note access

--- src/AppBundle/Security/Authorization/Voter/NoteVoter.php
+++ src/AppBundle/Security/Authorization/Voter/NoteVoter.php
@@ -9,6 +9,75 @@
 namespace AppBundle\Security\Authorization\Voter;
 
 
-class NoteVoter {
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
+class NoteVoter implements VoterInterface
+{
+    const VIEW = 'view';
+    const EDIT = 'edit';
+    public function supportsAttribute($attribute)
+    {
+        return in_array($attribute, array(
+            self::VIEW,
+            self::EDIT,
+        ));
+    }
+
+    public function supportsClass($class)
+    {
+        $supportedClass = 'AppBundle\Entity\Note';
+
+        return $supportedClass === $class || is_subclass_of($class, $supportedClass);
+    }
+
+
+    public function vote(TokenInterface $token, $note, array $attributes)
+    {
+        if (!$this->supportsClass(get_class($note))) {
+            return VoterInterface::ACCESS_ABSTAIN;
+        }
+
+
+        if (1 !== count($attributes)) {
+            throw new \InvalidArgumentException(
+                'Only one attribute is allowed for VIEW or EDIT'
+            );
+        }
+
+        $attribute = $attributes[0];
+
+        if (!$this->supportsAttribute($attribute)) {
+            return VoterInterface::ACCESS_ABSTAIN;
+        }
+
+        $user = $token->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return VoterInterface::ACCESS_DENIED;
+        }
+
+        switch($attribute) {
+            case self::VIEW:
+
+                if ($user->getId() === $note->getUSer()->getId()) {
+                    return VoterInterface::ACCESS_GRANTED;
+                }
+
+                if (!$note->getPrivate()) {
+                    return VoterInterface::ACCESS_GRANTED;
+                }
+                break;
+
+            case self::EDIT:
+
+                if ($user->getId() === $note->getUSer()->getId()) {
+                    return VoterInterface::ACCESS_GRANTED;
+                }
+                break;
+        }
+
+        return VoterInterface::ACCESS_DENIED;
+    }
 }
